711cb8995a95ae68bc415b22733f3e7a2472e54f
Viktor Lazarev <taurus101v@gmail.com>
2023-10-01 22:41:08 +0200

    Add custom implementation for unit tests
    https://os.phil-opp.com/testing/

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -11,6 +11,7 @@ lazy_static! {
     });
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
@@ -142,4 +143,22 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
 
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test println many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
 }
