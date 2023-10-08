e84df743832c0d0848ae21c59e297cd9cab1f491
Viktor Lazarev <taurus101v@gmail.com>
2023-10-08 22:26:53 +0200

    Add a hardware interrupts handler

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,20 @@ pub mod vga_buffer;
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+
+    unsafe {
+        interrupts::PICS.lock().initialize();
+    }
+
+    x86_64::instructions::interrupts::enable();
 }
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
+}
+
 pub trait Testable {
     fn run(&self) -> ();
 }
@@ -46,8 +59,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("Error: {}\n", info);
 
     exit_qemu(QemuExitCode::Failed);
-
-    loop {}
+    hlt_loop();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -72,8 +84,7 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
